6545ac69f620030d60a45567d92ad37f4f203dbf
Rebecca Eom <mye391@gmail.com>
2018-08-08 11:53:40 -0400

    feat(test): create tests for easy practice problems for users to test their solution against

--- practiceProblems/lastDigit/lastDigit.spec.js
+++ practiceProblems/lastDigit/lastDigit.spec.js
@@ -1,26 +1,37 @@
+const { expect } = require('chai')
+
+function lastDigit(num1, num2) {
+	num1 = num1.toString();
+	num2 = num2.toString();
+
+	if (num1[num1.length - 1] === num2[num2.length - 1]) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
 describe("lastDigit", function () {
-	xit("lastDigit is a function", function () {
-		var functionType = typeof lastDigit;
-
-		expect(functionType).toEqual("function");
+	it("lastDigit is a function", function () {
+		expect(lastDigit).to.be.a("function");
 	});
 
-	xit("lastDigit returns a boolean value (true or false)", function () {
-		var returnValue = lastDigit(22, 53),
-			booleanType = typeof returnValue;
+	it("lastDigit returns a boolean value (true or false)", function () {
+		var returnValue = lastDigit(22, 53)
 
-		expect(booleanType).toEqual("boolean");
+		expect(returnValue).to.be.a("boolean");
 	});
 
-	xit("if the two number arguments passed to lastDigit have the same last two digits (ones column) return the boolean value true", function () {
+	it("if the two number arguments passed to lastDigit have the same last two digits (ones column) return the boolean value true", function () {
 		var returnValue = lastDigit(352, 7892);
 
-		expect(returnValue).toEqual(true);
+		expect(returnValue).to.equal(true);
 	});
 
-	xit("if the last two digits (ones column) of the number arguments passed to last digit are NOT the same return the boolean value false", function () {
+	it("if the last two digits (ones column) of the number arguments passed to last digit are NOT the same return the boolean value false", function () {
 		var returnValue = lastDigit(357, 7894);
 
-		expect(returnValue).toEqual(false);
+		expect(returnValue).to.equal(false);
 	});
 });
