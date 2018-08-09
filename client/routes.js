ab4e20f6f4e629b632ea3d0084278d41aa9e114e
Rebecca Eom <mye391@gmail.com>
2018-08-09 17:53:39 -0400

    feat(editor): add editor component

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import Editor from './components/editor';
 
 /**
  * COMPONENT
@@ -21,6 +22,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route exact path="/editor" component={Editor} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
