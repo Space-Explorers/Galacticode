a6c6d6dea232fdae6f65934964bee9038259c8e6
Rebecca Eom <mye391@gmail.com>
2018-08-14 16:41:21 -0400

    update(store): create store for challenge component

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import results from './results'
+import challenge from './challenge'
 
-const reducer = combineReducers({user, results})
+const reducer = combineReducers({user, results, challenge})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -14,3 +15,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './results'
+export * from './challenge'
