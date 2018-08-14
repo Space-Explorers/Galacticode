d1a9cf1eadb7a4c7277a2a308ef229f04cb7381f
DC-1680 <dbconcha@gmail.com>
2018-08-14 12:57:40 -0400

    Update(Ask Politely): Component reverted back to original

--- client/components/askPolitely.js
+++ client/components/askPolitely.js
@@ -25,54 +25,27 @@ class AskPolitely extends Component {
   }
 
   render() {
-    console.log('RESULTS', this.props.results)
     return (
       <div>
         <h1>Ask Politely</h1>
-        <div id='main-container'>
-          <div id='prompt'>
-            <div>
+        <div>
+          <div>
+            <p>
               Create the function askPolitely that accepts a sentence as an
               argument.If the last character of the sentence is a question mark,
               then make sure the question ends with the word "please?". <br />
               If a question is already polite(meaning it already ends with
               "please") or the sentence is not a question, then return the
               inputted string without modification.
-            </div>
-            <div>
-              Examples:
-            </div>
-            <div>
-              <table>
-
-                <thead>
-                  <tr>
-                    <th>Input</th>
-                    <th>Expected Output</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>askPolitely("May I borrow your pencil?")</td>
-                    <td>"May I borrow your pencil please?"</td>
-                  </tr>
-                  <tr>
-                    <td>askPolitely("May I ask a question please?")</td>
-                    <td>"May I ask a question please?"</td>
-                  </tr>
-                  <tr>
-                    <td>askPolitely("My name is Grace Hopper.")</td>
-                    <td>"My name is Grace Hopper."</td>
-                  </tr>
-
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div id="inputResults">
-            <div id="editor">
-              <Editor onChange={this.onChange} value={this.state.value} />
-            </div>
+              <br />
+              Examples: - INPUT: askPolitely("May I borrow your pencil?"); -
+              OUTPUT: "May I borrow your pencil please?"
+              <br />
+              - INPUT: askPolitely("May I ask a question please?"); - OUTPUT:
+              "May I ask a question please?<br />
+              - INPUT: askPolitely("My name is Grace Hopper."); - OUTPUT: "My
+              name is Grace Hopper.";
+            </p>
             <div id="results">
               {
                 this.props.results &&
@@ -85,16 +58,18 @@ class AskPolitely extends Component {
             </div>
           </div>
           <div>
-            <button type="submit" onClick={this.handleSubmit}>
-              SUBMIT
-            </button>
+            <Editor onChange={this.onChange} value={this.state.value} />
           </div>
+        </div>
+        <div>
+          <button type="submit" onClick={this.handleSubmit}>
+            SUBMIT
+          </button>
         </div>
       </div>
     )
   }
 }
-
 const mapState = state => ({
   user: state.user,
   results: state.results
