de356d35d04333df470b03905e0c96d4c74f790c
Rebecca Eom <mye391@gmail.com>
2018-08-16 17:52:28 -0400

    update(api): optimize docker api post route

--- server/api/docker.js
+++ server/api/docker.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const axios = require('axios')
-const { Challenge } = require('../db/models')
+const { Challenge, User } = require('../db/models')
 
 module.exports = router
 
@@ -8,8 +8,9 @@ router.post('/', async (req, res, next) => {
   try {
 
     const specsFromDB = await Challenge.findById(req.body.problemId, {
-      attributes: ['specs']
+      attributes: ['specs', 'points']
     })
+
     const { data } = await axios.post(
       // 'http://localhost:8081/',
       'https://space-explorers-api.herokuapp.com/',
@@ -18,11 +19,41 @@ router.post('/', async (req, res, next) => {
         specs: specsFromDB.specs
       }
     )
+
     const responseInfo = JSON.parse(Buffer.from(data))
     const results = {
       stats: responseInfo.stats,
       tests: responseInfo.suites.suites[0].tests
     }
+
+    const challengeStatus = await axios.get(`/api/users/${req.user.id}/challenges/${req.body.problemId}`) // returns true or false
+
+    if (results.stats.passPercent === 100 && !challengeStatus.data) {
+      const points = specsFromDB.points
+      const problemId = req.body.problemId
+
+      const user = await User.findById(req.user.id, {
+        attributes: ['progress']
+      })
+
+      const progress = +points + user.progress
+
+      const [numberOfAffectedRows, affectedRows] = await User.update(
+        {
+          progress
+        },
+        {
+          where: { id: req.user.id },
+          returning: true,
+          plain: true
+        }
+      )
+      await affectedRows.addChallenge(problemId)
+    } else {
+      const failing = results.tests.filter(test => test.pass === 'false')
+      const errMsgs = failing.forEach(error => {return error.err.message})
+    }
+
     res.json(results)
   } catch (err) {
     next(err)
