6a46f462370d57251526ae4778866e1cd9a67284
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-18 17:58:58 -0400

    commit to switch branches

--- script/specs/greeting.spec.js
+++ script/specs/greeting.spec.js
@@ -1,27 +1,27 @@
-describe('greeting', function () {
+const {expect} = require('chai')
 
-	it('greeting is a function', function () {
-		var functionType = typeof greeting;
-		expect(functionType).toEqual('function');
-	})
+describe('greeting', function() {
+  it('greeting is a function', function() {
+    var functionType = typeof greeting
+    expect(functionType).toEqual('function')
+  })
 
-	it('returns a string value', function () {
-		var returnValue = greeting(),
-			stringType = typeof returnValue;
+  it('returns a string value', function() {
+    var returnValue = greeting(),
+      stringType = typeof returnValue
 
-		expect(stringType).toEqual('string');
-	})
+    expect(stringType).toEqual('string')
+  })
 
-	it('returns the string "Hello!" when an argument is NOT passed', function () {
-		var returnValue = greeting();
+  it('returns the string "Hello!" when an argument is NOT passed', function() {
+    var returnValue = greeting()
 
-		expect(returnValue).toEqual("Hello!");
-	})
+    expect(returnValue).toEqual('Hello!')
+  })
 
-	it('returns the string "Hello, Joe!" when an argument IS passed', function () {
-		var returnValue = greeting('Joe');
+  it('returns the string "Hello, Joe!" when an argument IS passed', function() {
+    var returnValue = greeting('Joe')
 
-		expect(returnValue).toEqual('Hello, Joe!');
-	})
-
+    expect(returnValue).toEqual('Hello, Joe!')
+  })
 })
