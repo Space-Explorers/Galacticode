92fcba1878052c11fb0052d13100f9d1bdd3dee5
Katherine Pangtay <kpangtay@gmail.com>
2018-08-20 10:24:14 -0400

    rolling ball working

--- client/components/threejs/gameplay.js
+++ client/components/threejs/gameplay.js
@@ -1,166 +1,100 @@
 import * as THREE from 'three'
+// import * as CANNON from 'cannon'
+
+
 
 export default function gamePlayEnvironment() {
-  let particles
+  let alienRollingSpeed=(0.008 * 26 / 0.2)/5
+  let bounceValue = 0.01
+  let gravity=0.005
+  let leftLane=-1
+  let rightLane=1
+  let middleLane=0
+  let currentLane
 
-     // Create an empty scene, camera
-  const scene = new THREE.Scene()
-  scene.background = new THREE.Color( 0x252940 );
+      const clock = new THREE.Clock()
+      clock.start()
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color( 0x252940 );
+      const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
+      camera.position.z = 6.5;
+      camera.position.y = 2.5;
 
-  const camera = new THREE.PerspectiveCamera( 25,window.innerWidth / window.innerHeight, 1, 1000)
-  camera.position.x = 0
-  camera.position.y = 170
-  camera.position.z = 900
-  // camera.lookAt(scene.position)
+      const renderer = new THREE.WebGLRenderer({alpha: true})
+      renderer.shadowMap.enabled = true;//enable shadow
+      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+      renderer.setClearColor(0x000000, 0)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(renderer.domElement)
 
-  // Create a renderer
-  const renderer = new THREE.WebGLRenderer({antialias: true})
+      // //initialize Cannon World
+      // const world = new CANNON.World()
+      // world.gravity.set(0,0,0)
+      // world.broadphase = new CANNON.NaiveBroadphase()
 
-  renderer.setClearColor(0x000000, 0)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
-  // Append Renderer to DOM
-  document.body.appendChild(renderer.domElement)
-
-  //resize window
-  window.addEventListener(
-    'resize',
-    function() {
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-    },
-    false
-  );
-
-  // Stars in the backgrounds
-  const drawStars = function() {
-    let canvas, ctx, i, j, sizeRandom;
-    canvas = document.createElement('canvas');
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-    canvas.setAttribute('id', "stars");
-    ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#A9D9E5";
-    for (i = j = 0; j <= 200; i = ++j) {
-      ctx.beginPath();
-      sizeRandom = Math.random() * 2;
-      ctx.arc(Math.random() * window.innerWidth, Math.random() * window.innerHeight, sizeRandom, 0, 2 * Math.PI, 0);
-      ctx.fill();
-    }
-    return document.body.appendChild(canvas);
-  };
-
-  //flying particles
-  function drawParticles() {
-     particles = new THREE.Group();
-    scene.add(particles);
-    const geometry = new THREE.TetrahedronGeometry(2, 0);
-
-    for (let i = 0; i < 500; i ++) {
-      const material = new THREE.MeshPhongMaterial({
-        color: 0x9CE7EA,
-        shading: THREE.FlatShading
-      });
-      const mesh = new THREE.Mesh(geometry, material);
-          mesh.position.set((Math.random() - 0.5) * 1000,
-          (Math.random() - 0.5) * 1000,
-          (Math.random() - 0.5) * 1000);
-          mesh.updateMatrix();
-          mesh.matrixAutoUpdate = false;
-      particles.add(mesh);
-    }
-  }
-
-  // Create Main Planet
-  const mesh = new THREE.Mesh(
-    //dictate the size of the planet (10 -17)
-    new THREE.IcosahedronGeometry(300,1),
-    new THREE.MeshPhongMaterial({
-      color: 0x205BF8,
-      emissive: 0x0E24F3,
-      side: THREE.DoubleSide,
-      flatShading: true,
-    }),
-  )
-  mesh.rotation.set(0.4,0.3,0)
-  mesh.receiveShadow = true
-  mesh.position.set(0, -190, 0)
-
-  // Create Secondary Planet
-    const otherPlanet = new THREE.Mesh(
-      //dictate the size of the planet (10 -17)
-      new THREE.IcosahedronGeometry(90,1),
-      new THREE.MeshPhongMaterial({
-        color: 0x2B1255,
-        emissive: 0x995E78,
-        side: THREE.DoubleSide,
-        flatShading: true,
-      }),
-    )
-    otherPlanet.rotation.set(0.4,0.3,0)
-    otherPlanet.receiveShadow = true
-    otherPlanet.position.set(270, 190, 90)
-
-  // Create Third Planet
-    const thirdPlanet = new THREE.Mesh(
-      //dictate the size of the planet (10 -17)
-      new THREE.IcosahedronGeometry(40,1),
-      new THREE.MeshPhongMaterial({
-        color: 0xCD4537,
-        emissive: 0xCD4537,
-        side: THREE.DoubleSide,
-        flatShading: true,
-      }),
-    )
-    thirdPlanet.rotation.set(0.4,0.3,0)
-    thirdPlanet.receiveShadow = true
-    thirdPlanet.position.set(-250, 300, 90)
-
-
-  drawStars()
-  drawParticles()
+      //Alien
+      const alienGeometry = new THREE.DodecahedronGeometry( 0.2,1)
+      const alienMaterial = new THREE.MeshStandardMaterial( { color: 0x883333, shading: THREE.FlatShading } )
+      //const jumping = false
+      const alien = new THREE.Mesh( alienGeometry, alienMaterial );
+      alien.castShadow=true;
+      alien.receiveShadow=false
+      alien.position.y=1.8
+      alien.position.z=4.8
+      currentLane = middleLane
+      alien.position.x = currentLane
+      scene.add( alien )
+      // //createPlanet
+        const planetGeometry = new THREE.TetrahedronBufferGeometry(500,4)
+        const planetMaterial = new THREE.MeshStandardMaterial({
+          color: 0x205BF8,
+          shading: THREE.FlatShading
+        })
+        const planet = new THREE.Mesh(planetGeometry, planetMaterial)
+        planet.receiveShadow = true
+        planet.rotation.z =- Math.PI/2
+        scene.add(planet)
+        planet.position.y =-24
+        planet.position.x=2
 
 
 
-   //lighting
+        // const orbitControl = new THREE.OrbitControls( camera, renderer.domElement );
+        // orbitControl.addEventListener( 'change', render );
+        // orbitControl.enableZoom = false;
+
+
+
+       //lighting
    const ambientLight = new THREE.AmbientLight(0x999999)
    scene.add(ambientLight)
 
     const light = new THREE.DirectionalLight(0xffffff, 1.5)
-    light.position.set(200,100,200);
+    light.position.set(0,4,1);
     light.castShadow = true;
 
     scene.add(light)
 
-
-//shows in which direction the lighting is coming from
-  // const helper = new THREE.CameraHelper(light.shadow.camera)
-  // scene.add(helper)
-
-  // Add mesh to Scene
-    scene.add(mesh)
-    scene.add(otherPlanet)
-    scene.add(thirdPlanet)
-
-
-  // Render Loop
+      // Render Loop
   function render(){
     renderer.render(scene, camera)
   }
 
   function animate(){
     requestAnimationFrame(animate)
+    alien.rotation.x -= alienRollingSpeed
+    if(alien.position.y <= 1.8){
+      bounceValue=(Math.random()*0.04)+0.005
+    }
+    alien.position.y += bounceValue
+    // alien.position.x = THREE.Math.lerp(alien.position.x, currentLane, 2*clock.getDelta())
+    bounceValue -= gravity
+    planet.rotation. x += 0.008
     render()
-    particles.rotation.x += 0.001;
-    particles.rotation.y -= 0.001;
-    mesh.rotation.y += 0.003
-    otherPlanet.rotation.y += 0.003
-    thirdPlanet.rotation.y += 0.003
-
   }
 
   animate()
+
 //END
   }
 
