2dac25d4740e58d3117ab22b8d75423b77d2f63b
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-20 11:33:17 -0400

    data is now cleared from challenge and results components when unmounting

--- client/store/results.js
+++ client/store/results.js
@@ -3,20 +3,24 @@ import axios from 'axios'
 // ACTION TYPES
 
 const GOT_RESULTS = 'GOT_RESULTS'
+const REMOVED_RESULTS = 'REMOVED_RESULTS'
 
 // ACTION CREATORS
 
-const gotResults = (results) => ({
+const gotResults = results => ({
   type: GOT_RESULTS,
   results
 })
 
+const removedResults = () => ({
+  type: REMOVED_RESULTS
+})
+
 // THUNK CREATORS
 
-export const getResults = ( code, problemId ) =>
-async dispatch => {
+export const getResults = (code, problemId) => async dispatch => {
   try {
-    const { data } = await axios.post('/api/docker', {
+    const {data} = await axios.post('/api/docker', {
       code,
       problemId
     })
@@ -27,15 +31,23 @@ async dispatch => {
   }
 }
 
+export const removeResultsData = () => dispatch => {
+  try {
+    dispatch(removedResults())
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 // REDUCER
 
 export default function(state = {}, action) {
   switch (action.type) {
     case GOT_RESULTS: {
-      return {
-        ...state,
-        results: action.results
-      }
+      return action.results
+    }
+    case REMOVED_RESULTS: {
+      return {}
     }
     default:
       return state
