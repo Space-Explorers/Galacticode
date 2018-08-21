13047ce66c61d68fe9de8a6db74b5ed2786eddc4
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-21 11:22:56 -0400

    fixed logo image links that were breaking on refresh and logout

--- client/components/main.js
+++ client/components/main.js
@@ -6,7 +6,7 @@ const Main = () => {
     <div>
       {planetBackground()}
       <div id="login-wrapper">
-        <img id="login-logo" src="./GALACTICODE-logo_blue.png" />
+        <img id="login-logo" src="/GALACTICODE-logo_blue.png" />
         <div>
           <p id="intro">
             Our friendly alien is lost in space and needs your help! Explore new
