07c2df61364ae0cadf88e2e4d3647d8a847fac94
Rebecca Eom <mye391@gmail.com>
2018-08-21 12:35:05 -0400

    feat(update): style victory popup component

--- client/components/victory.js
+++ client/components/victory.js
@@ -20,12 +20,24 @@ class Victory extends Component {
     return (
       <div>
         <Popup
+          contentStyle={{
+            background: "none",
+            border: "none",
+            position: "absolute",
+            margin: "auto",
+            top: "200px",
+            right: "0",
+            left: "0",
+            width: "700px"
+          }}
           open={this.state.open}
           onClose={this.closeModal}
         >
           <div className="modal">
-            <button className="btn btn-close" onClick={this.closeModal}>Close</button>
-            <div className="header"> CONGRATULATIONS! </div>
+            <div className="close">
+              <button className="btn btn-close" onClick={this.closeModal}>Close</button>
+            </div>
+            <div className="header"><h1>CONGRATULATIONS!</h1></div>
             <div className="content">
               <p>
                 Well done! You now have enough fuel to travel to the next planet!<br />Would you like to continue?
@@ -33,10 +45,10 @@ class Victory extends Component {
             </div>
             <div className="actions">
               <Link to="/">
-                <button type="button" onClick={this.closeModal}> Yes, take me away!</button>
+                <button type="button" className="btn btn-modal" onClick={this.closeModal}>Blast Off!</button>
               </Link>
               <Link to="/play">
-                <button type="button" onClick={this.closeModal}>No, I'll stay.</button>
+                <button type="button" className="btn btn-modal" onClick={this.closeModal}>Not Yet</button>
               </Link>
             </div>
           </div>
