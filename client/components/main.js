b8ede97602dc3c70e5837037870521070c9e48d4
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-21 11:11:15 -0400

    changed login buttons to overall theme, adjusted flex spacing

--- client/components/main.js
+++ client/components/main.js
@@ -14,12 +14,15 @@ const Main = () => {
             new planets and find your way home.
           </p>
           <div id="login-buttons">
-            <a href="/auth/google">
-              <img src="btn_google_signin_light_normal_web.png" />
-              {/* Login with Google */}
-            </a>
-            <br />
-            <a href="/auth/github">Login with Github</a>
+            <button className="btn btn-login">
+              <a href="/auth/google">
+                {/* <img src="btn_google_signin_light_normal_web.png" /> */}
+                Login with Google
+              </a>
+            </button>
+            <button className="btn btn-login">
+              <a href="/auth/github">Login with Github</a>
+            </button>
           </div>
         </div>
       </div>
