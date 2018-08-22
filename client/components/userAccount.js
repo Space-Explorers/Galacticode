020fdf1f1bc5d7b0832598fedcf5177ebe4c883f
Amy De Genaro <amy.degenaro@gmail.com>
2018-08-21 20:26:29 -0400

    rearranged user account component to show improved solutions editor

--- client/components/userAccount.js
+++ client/components/userAccount.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {getSolvedData} from '../store'
+import {getSolvedData, getUnlockedPlanets} from '../store'
 import {Editor} from './index'
 
 class UserAccount extends Component {
@@ -11,19 +11,20 @@ class UserAccount extends Component {
       selectedSolution: ''
     }
   }
-  componentDidMount() {
-    this.props.fetchCompletedChallenges(this.props.user.id)
+  async componentDidMount() {
+    await this.props.fetchCompletedChallenges(this.props.user.id)
+    await this.props.fetchUnlockedPlanets(this.props.user.id)
   }
 
   render() {
-    const {user, completedChallenges, progress} = this.props
+    const {user, completedChallenges, progress, currentPlanet} = this.props
     if (user.id) {
       return (
-        <div className="main-wrapper">
+        <div className="account-wrapper">
           <div className="challenge-header">
             <div>
-              <h2>Player Name: {user.name || user.email}</h2>
-              <h2>Total Fuel Points: {progress}</h2>
+              <h2>PLAYER: {user.name || user.email}</h2>
+              <h2>TOTAL FUEL: {progress || 0}</h2>
             </div>
             <button
               className="btn btn-close"
@@ -53,13 +54,19 @@ class UserAccount extends Component {
             ) : (
               <p>You haven't solved any challenges yet. Get coding!</p>
             )}
-            <h3>Current Planet</h3>
-            <div>
-              <p>INSERT FURTHEST PLANET HERE</p>
-            </div>
           </div>
-          <div>
-            <Editor value={this.state.selectedSolution} />
+          <Editor
+            value={this.state.selectedSolution}
+            readOnly={true}
+            showLineNumbers={false}
+            showGutter={false}
+            height="150px"
+          />
+          <div id="user-planet">
+            <h3>Currently on Planet {currentPlanet.name}</h3>
+            <div>
+              <img width="150" height="150" src={currentPlanet.img} />
+            </div>
           </div>
         </div>
       )
@@ -68,16 +75,26 @@ class UserAccount extends Component {
 }
 
 const mapState = state => {
+  let furthestPlanet = {}
+  if (state.planets.unlockedPlanets) {
+    const userPlanets = state.planets.unlockedPlanets
+    const planetIds = userPlanets.map(planet => planet.id)
+    const maxPlanetId = Math.max(planetIds)
+    furthestPlanet = userPlanets.find(planet => planet.id === maxPlanetId)
+  }
+
   return {
     user: state.user,
     completedChallenges: state.solvedChallenges.completedChallenges,
-    progress: state.progress
+    progress: state.progress,
+    currentPlanet: furthestPlanet
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    fetchCompletedChallenges: id => dispatch(getSolvedData(id))
+    fetchCompletedChallenges: id => dispatch(getSolvedData(id)),
+    fetchUnlockedPlanets: id => dispatch(getUnlockedPlanets(id))
   }
 }
 
